e98f0692b7c2fa880ee6c2ab9092f257441f4854
RomanNikitenko <68384950+RomanNikitenko19@users.noreply.github.com>
2020-09-03 18:41:37 +0300

    module 1 part 1

---
+++ index.js
@@ -0,0 +1,37 @@
+// const name = "Alina";
+// let age = prompt('Введите ваш возраст');
+// console.log(typeof age);
+// console.log(age);
+// age = +age 
+// age = Number(age);
+// console.log(typeof age);
+// console.log(age);
+// const message = "Привет " + name;
+// console.log(message);
+// let newMessage = `My age is ${age}`;
+// console.log(newMessage);
+// let newAnswer = confirm(`Вам есть 18?`);
+// console.log(answel);
+// console.log(typeof answel);
+// let newQestion = `Вы любите js?`;
+// let newAnswer = confirm (newQestion);
+// console.log(newQestion);
+// console.log(newAnswer);
+
+
+let isOpen;
+console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolUnd = Boolean(isOpen);
+// console.log(boolUnd);
+// console.log(typeof boolUnd);
+
+// isOpen = null;
+// console.log(isOpen);
+// console.log(typeof isOpen);
+
+// let boolNull = Boolean (isOpen);
+// console.log(boolNull);
+// console.log(typeof boolNull);
+   
